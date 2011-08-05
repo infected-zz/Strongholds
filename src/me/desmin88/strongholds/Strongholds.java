486633b06d5c4de73f9361f7eccd662dfc0c2f4d
desmin88 <billyryan24_7@yahoo.com>
2011-08-05 16:00:36 -0400

    SQLite, table query done

--- src/me/desmin88/strongholds/Strongholds.java
+++ src/me/desmin88/strongholds/Strongholds.java
@@ -1,28 +1,22 @@
 package me.desmin88.strongholds;
 
-import java.net.MalformedURLException;
-import java.util.logging.Logger;
 
-import me.desmin88.strongholds.commands.SHCommands;
+import java.util.logging.Logger;
 
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.config.Configuration;
 
-import com.alta189.sqlLibrary.MySQL.mysqlCore;
+import com.alta189.sqlLibrary.SQLite.sqlCore;
 
 public class Strongholds extends JavaPlugin {
 	private Logger log = Logger.getLogger("Minecraft");
 	public static String pref = "[Strongholds] ";
-	public mysqlCore sqlCore = null;
-	private String dbHost = null;
-	private String dbUser = null;
-	private String dbPass = null;
-	private String dbDatabase = null;
+	public sqlCore Core = null;
 	private PluginDescriptionFile pdf;
-	private static SettingsManager sm;
 	private Configuration cfg;
+	public static String table = "CREATE TABLE strongholds ( name VARCHAR(255), leader VARCHAR(255), players TEXT, level INT, x INT, y INT, z INT, x2 INT, y2 INT, z2 INT, world VARCHAR(255), PRIMARY KEY(`name`));";
 	@Override
 	public void onDisable() {
 		System.out.println("[" + pdf.getName() + "]"  + " by " + pdf.getAuthors().get(0) + " version " + pdf.getVersion() + " disabled.");
@@ -31,36 +25,29 @@ public class Strongholds extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		pdf = this.getDescription();
-        sm = new SettingsManager(this); //Lets pull all of our values..
-		dbHost = sm.dbHost;
-		dbUser = sm.dbUser;
-		dbPass = sm.dbPass;
-		dbDatabase = sm.dbDatabase;
-		this.cfg = sm.cfg; //We can pull our cfg from sm's cfg.
+		getDataFolder().mkdir();
 		
-		System.out.println(pref + "Connecting to " +dbHost+ "@" + dbHost);
-		sqlCore = new mysqlCore(this.log, this.pref, this.dbHost, this.dbDatabase, this.dbUser, this.dbPass); // Make an instance of a sql core for use
-		System.out.println(pref + "Initializing MySQL core for connections");
-		sqlCore.initialize(); //We initialize to begin connections
+		Core = new sqlCore(this.log, this.pref, "Strongholds", getDataFolder().getPath()); // Make an instance of a sql core for use
+		System.out.println(pref + "Initializing SQL core for connections");
+		Core.initialize(); //We initialize to begin connections
 		//Code to basically establish a connection to the database.
 		try {
-			if(sqlCore.checkConnection()) { // Checking if we have made a valid connection
-				System.out.println(pref + "Valid MySQL connection made!");
-				if(!sqlCore.checkTable("")) { //Table not made. 
+			if(Core.checkConnection()) { // Checking if we have made a valid connection
+				System.out.println(pref + "Valid SQL connection made!");
+				if(!Core.checkTable(table)) { //Table not made. 
 					System.out.println(pref + "First run detected, making table for data entry.");
-					String query = ""; // To be filled in later
-					sqlCore.createTable(query); // Table not made, we now make it for data entry
+					Core.createTable(table); // Table not made, we now make it for data entry
 				}
 			}
 			else{ // We didn't connect, possibly wrong values in the config?!
-				System.out.println(pref + "Error checking for MySQL connection, aborting .");
+				System.out.println(pref + "Error checking for SQL connection, aborting .");
 			}
 		} catch (Exception e) { // Odd exceptions, hmm. 
-			System.out.println(pref + "Error checking for MySQL connection, aborting.");
+			System.out.println(pref + "Error checking for SQL connection, aborting.");
 			e.printStackTrace();
 		}
 		PluginManager pm = this.getServer().getPluginManager();
-		getCommand("sh").setExecutor(new SHCommands());
+		//getCommand("sh").setExecutor(new SHCommands());
 		System.out.println("[" + pdf.getName() + "]"  + " by " + pdf.getAuthors().get(0) + " version " + pdf.getVersion() + " enabled.");
 	
 	}
